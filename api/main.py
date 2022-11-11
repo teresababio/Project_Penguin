e8aa32821edb9bd5e7ba2d8e42fa99ae00276b4c
teresababio <teresa.babio@gmail.com>
2022-11-11 17:52:44 +0000

    Api main.py y carga de datos

--- api/main.py
+++ api/main.py
@@ -1,1 +1,22 @@
 from fastapi import FastAPI
+from bson import json_util
+from json import loads
+from routers import penguin
+
+app = FastAPI()
+
+
+#Incluimos los endpoints de los ficheros
+app.include_router(penguin.router)
+
+@app.get("/")
+def raiz():
+    return {
+        "message":"Bienvenido a la Api de los Pingüinos"
+    }
+
+@app.get("info")
+def info():
+    return{
+        #explicar brevemente los router
+    }
